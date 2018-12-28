c01dc8e0d62b09c6293fc8b9459b172ba69d02a2
Masaya Kataoka <ms.kataoka@gmail.com>
2018-12-28 10:39:06 +0900

    add error message

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import render_template
 import glob
 import os
 import rosbag
+import decimal
 
+bagfile_paths = glob.glob('/home/masaya/robotx_data/all/*.bag')
 app = Flask(__name__)
 
 def read_rosbag(path):
@@ -11,16 +13,21 @@ def read_rosbag(path):
     return
  
 @app.route('/')
-def hello_world():
-    bagfile_paths = glob.glob('/home/masaya/robotx_data/all/*.bag')
+def show_index():
     bagfiles = []
     sizes = []
     for path in bagfile_paths:
         bagfiles.append(os.path.basename(path))
-        size = str(os.path.getsize(path)/1073741824.0)+' GB'
+        size = "{:.2f}".format(os.path.getsize(path)/1073741824.0) + " GB"
         sizes.append(size)
     bagdatas = [{"filename":filename, "path":path} for (filename, path) in zip(bagfiles, bagfile_paths)]
     return render_template('index.html',bagfiles=bagfiles,sizes=sizes)
- 
+
+@app.route("/rosbag/id=<int:id>/")
+def show_rosbag_page(id):
+    if id > len(bagfile_paths):
+        return render_template('no_rosbag.html')
+    return render_template('rosbag.html')
+
 if __name__ == '__main__':
     app.run()
